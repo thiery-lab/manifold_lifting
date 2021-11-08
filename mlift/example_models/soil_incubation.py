3225e62d21d193bfff81461cbaaa1ca53edfac9f
Matt Graham <matthew.m.graham@gmail.com>
2021-11-08 22:26:15 +0000

    Reparametrizing soil model + exponential integrator

--- mlift/example_models/soil_incubation.py
+++ mlift/example_models/soil_incubation.py
@@ -8,8 +8,8 @@ import numpy as onp
 import jax.config
 import jax.numpy as np
 from mlift.systems import IndependentAdditiveNoiseModelSystem
-from mlift.distributions import half_normal, half_cauchy, beta
-from mlift.ode import integrate_ode_rk4
+from mlift.distributions import half_normal, uniform, log_normal
+from mlift.ode import integrate_ode_expm
 from mlift.prior import PriorSpecification, set_up_prior
 import mlift.example_models.utils as utils
 
@@ -18,17 +18,37 @@ jax.config.update("jax_platform_name", "cpu")
 
 
 prior_specifications = {
-    "k_1": PriorSpecification(distribution=half_normal(1)),
-    "k_2": PriorSpecification(distribution=half_normal(1)),
-    "α_21": PriorSpecification(distribution=half_normal(1)),
-    "α_12": PriorSpecification(distribution=half_normal(1)),
-    "γ": PriorSpecification(distribution=beta(10, 1)),
-    "σ": PriorSpecification(distribution=half_cauchy(1)),
+    "u_1": PriorSpecification(distribution=uniform(0, 1)),
+    "u_2": PriorSpecification(distribution=uniform(0, 1)),
+    "α_12": PriorSpecification(distribution=uniform(0, 1)),
+    "α_21_over_1_minus_α_12": PriorSpecification(distribution=uniform(0, 1)),
+    "γ": PriorSpecification(distribution=uniform(0, 1)),
+    "C_0": PriorSpecification(distribution=log_normal(1, 2)),
+    "σ": PriorSpecification(distribution=half_normal(1)),
 }
 
-compute_dim_u, generate_params, prior_neg_log_dens, sample_from_prior = set_up_prior(
+compute_dim_u, _generate_params, prior_neg_log_dens, sample_from_prior = set_up_prior(
     prior_specifications
 )
+
+
+def generate_params(u, data):
+    params = _generate_params(u, data)
+    # k_1 ~ half_normal(1), k_2 ~ half_normal(1) subject to k_2 <= k_1
+    # use polar reparameterisation with k_1 = r * cos(θ), k_2 = r * sin(θ) and
+    # r ~ rayleigh(1) and θ ~ uniform(0, π / 4)
+    # equivalent to
+    # r = sqrt(-2 * log(u_1)) and θ = u_2 * π / 4
+    # with u_1 ~ uniform(0, 1) and u_2 ~ uniform(0, 1)
+    u_1, u_2 = params.pop("u_1"), params.pop("u_2")
+    r = (-2 * np.log(u_1))**0.5
+    θ = u_2 * np.pi / 4
+    params["k_1"] = r * np.cos(θ)
+    params["k_2"] = r * np.sin(θ)
+    # α_12 ~ uniform(0, 1), α_21 ~ uniform(0, 1) subject to α_12 + α_21 <= 1
+    α_21_over_1_minus_α_12 = params.pop("α_21_over_1_minus_α_12")
+    params["α_21"] = α_21_over_1_minus_α_12 * (1 - params["α_12"])
+    return params
 
 
 def dx_dt(x, t, params):
@@ -40,25 +60,29 @@ def dx_dt(x, t, params):
     )
 
 
-def observation_func(x, data):
-    return data["initial_total_carbon"] - x[1:].sum(-1)
+def jacob_dx_dt(params):
+    return np.array(
+        [
+            [-params["k_1"], params["α_12"] * params["k_2"]],
+            [params["α_21"] * params["k_1"], -params["k_2"]],
+        ]
+    )
+
+
+def observation_func(x, params):
+    return params["C_0"] - x[1:].sum(-1)
 
 
 def generate_from_model(u, data):
     params = generate_params(u, data)
-    x_init = np.array(
-        (
-            params["γ"] * data["initial_total_carbon"],
-            (1 - params["γ"]) * data["initial_total_carbon"],
-        )
-    )
-    x = integrate_ode_rk4(dx_dt, x_init, data["t_seq"], params, data["dt"])
+    x_init = np.array((params["γ"] * params["C_0"], (1 - params["γ"]) * params["C_0"],))
+    x = integrate_ode_expm(jacob_dx_dt(params), x_init, data["t_seq"])
     return params, x
 
 
 def generate_y(u, n, data):
     params, x = generate_from_model(u, data)
-    return observation_func(x, data) + params["σ"] * n
+    return observation_func(x, params) + params["σ"] * n
 
 
 def extended_prior_neg_log_dens(q, data):
@@ -69,7 +93,7 @@ def extended_prior_neg_log_dens(q, data):
 
 def posterior_neg_log_dens(u, data):
     params, x = generate_from_model(u, data)
-    y_mean = observation_func(x, data)
+    y_mean = observation_func(x, params)
     return (
         prior_neg_log_dens(u, data)
         + (
@@ -86,7 +110,7 @@ def sample_initial_states(
     while len(init_states) < num_chain:
         u = sample_from_prior(rng, data)
         params, x = generate_from_model(u, data)
-        y_mean = observation_func(x, data)
+        y_mean = observation_func(x, params)
         if not onp.all(np.isfinite(y_mean)):
             continue
         n = (data["y_obs"] - y_mean) / params["σ"]
@@ -108,11 +132,21 @@ if __name__ == "__main__":
     parser = utils.set_up_argparser_with_standard_arguments(
         "Run soil-incubation model experiment"
     )
+    parser.add_argument(
+        "--dataset",
+        default="ak-t35",
+        choices=("hn-t25", "ak-t35"),
+        help="Which dataset to perform inference with",
+    )
     args = parser.parse_args()
 
     # Load data
-
-    data = dict(onp.load(os.path.join(args.data_dir, "soil-incubation-data.npz")))
+    data = dict(
+        onp.load(
+            os.path.join(args.data_dir, f"soil-incubation-{args.dataset}-data.npz")
+        )
+    )
+    data["y_obs"] = data["y_mean"]
     dim_u = compute_dim_u(data)
 
     # Set up seeded random number generator
@@ -125,12 +159,14 @@ if __name__ == "__main__":
 
     # Run experiment
 
+    var_names = ["k_1", "k_2", "α_12", "α_21", "γ", "C_0", "σ"]
+
     final_states, traces, stats, summary_dict, sampler = utils.run_experiment(
         args=args,
         data=data,
         rng=rng,
-        experiment_name="soil_incubation",
-        var_names=list(prior_specifications.keys()),
+        experiment_name=f"soil_incubation_{args.dataset}",
+        var_names=var_names,
         var_trace_func=trace_func,
         posterior_neg_log_dens=posterior_neg_log_dens,
         extended_prior_neg_log_dens=extended_prior_neg_log_dens,
@@ -142,4 +178,3 @@ if __name__ == "__main__":
         },
         sample_initial_states=sample_initial_states,
     )
-
